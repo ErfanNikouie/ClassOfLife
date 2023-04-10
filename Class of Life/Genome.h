66c0d0cbe077de9efc0b8e3d85678dfcb425f575
Erfan Nik <er.nikouie@gmail.com>
2023-04-10 19:07:00 +0330

    Starting Point

---
+++ Class of Life/Genome.h
@@ -0,0 +1,115 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+typedef std::string string;
+typedef std::pair<string, string> DNAPair;
+
+class Genome
+{
+private:
+	DNAPair DNA;
+	string RNA;
+
+
+	static char GetCharCompliment(char c)
+	{
+		if (c == 'A')
+			return 'T';
+
+		if (c == 'T')
+			return 'A';
+
+		if (c == 'G')
+			return 'C';
+
+		if (c == 'C')
+			return 'G';
+	}
+	static string GetStringCompliment(string s)
+	{
+		string c = s;
+		for (int i = 0; i < c.length(); i++)
+			c[i] = GetCharCompliment(c[i]);
+
+		return c;
+	}
+
+public:
+
+	Genome(string rna)
+	{
+		RNA = rna;
+		DNA.first = rna;
+		DNA.second = GetStringCompliment(DNA.first);
+	}
+
+	DNAPair GetDNA()
+	{
+		return DNA;
+	}
+
+	string GetRNA()
+	{
+		return RNA;
+	}
+
+	/// <summary> DNA Small Mutation. Changes every 'a' to 'b', 'n' times.</summary>
+	void PointMutationDNA(char a, char b, int n)
+	{
+		int iterator = 0;
+		for (int i = 0; i < DNA.first.length(); i++)
+		{
+			if (DNA.first[i] == a)
+			{
+				DNA.first[i] = b;
+				DNA.second[i] = GetCharCompliment(b);
+			}
+
+			if (DNA.second[i] == a)
+			{
+				DNA.second[i] = b;
+				DNA.first[i] = GetCharCompliment(b);
+			}
+
+			if ((DNA.first[i] == a) || (DNA.second[i] == a))
+				iterator++;
+
+			if (iterator == n)
+				break;
+		}
+	}
+
+	/// <summary> RNA Small Mutation. Changes every 'a' to 'b', 'n' times.</summary>
+	void PointMutationRNA(char a, char b, int n)
+	{
+		int iterator = 0;
+		for (int i = 0; i < DNA.first.length(); i++)
+		{
+			if (RNA[i] == a)
+			{
+				RNA[i] = b;
+				iterator++;
+			}
+
+			if (iterator == n)
+				break;
+		}
+	}
+
+	void LargeMutationDNA(string a, string b)
+	{
+
+	}
+
+	void LargeMutationRNA(string a, string b)
+	{
+
+	}
+	
+	static string CreateDNA(string rna)
+	{
+		return GetStringCompliment(rna);
+	}
+};
+
